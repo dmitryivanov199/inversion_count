10b701dd460b78aa979b4995dfda8b755e5e41c4
dmitryivanov199 <dmitryivanov199@mail.ru>
2022-09-21 20:14:07 +0300

    Fix half array getting, improve testing

--- src/inversion_count.cpp
+++ src/inversion_count.cpp
@@ -6,7 +6,7 @@ static unsigned int sort_and_count_inversions(int *a, unsigned int n);
 
 static bool is_base_case(unsigned int n);
 
-static void get_array_half(const int *a, int *part, unsigned int n, half which_half);
+static void get_array_half(const int *a, int *array_half, unsigned int n, unsigned int half_n, half which_half);
 
 static unsigned int merge_and_count_inversions(int *b, unsigned int n, const int *c, unsigned int c_n, const int *d, unsigned int d_n);
 
@@ -35,8 +35,8 @@ unsigned int sort_and_count_inversions(int *a, unsigned int n) {
     const unsigned int d_n{n - c_n};
     int c[c_n];
     int d[d_n];
-    get_array_half(a, c, n, half::LEFT);
-    get_array_half(a, d, n, half::RIGHT);
+    get_array_half(a, c, n, c_n, half::LEFT);
+    get_array_half(a, d, n, d_n, half::RIGHT);
 
     unsigned int left_inversions_count{sort_and_count_inversions(c, c_n)};
     unsigned int right_inversions_count{sort_and_count_inversions(d, d_n)};
@@ -49,13 +49,12 @@ bool is_base_case(unsigned int n) {
     return n <= 1;
 }
 
-void get_array_half(const int *a, int *part, unsigned int n, half which_half) {
+void get_array_half(const int *a, int *array_half, unsigned int n, unsigned int half_n, half which_half) {
     unsigned int i{(which_half == half::LEFT)? 0 : n / 2};
-    unsigned int j{0};
 
-    for (; i < n; i++) {
-        part[j] = a[i];
-        j++;
+    for (unsigned int j{0}; j < half_n; j++) {
+        array_half[j] = a[i];
+        i++;
     }
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 
 bool run_tests();
 
+unsigned int get_random_array_number();
+
+void fill_array(int *a, unsigned int n);
+
+unsigned int simple_count_inversions(const int *a, unsigned int n);
+
 int main () {
     if (run_tests()) {
         std::cout << "All tests passed!" << std::endl;
@@ -18,44 +24,48 @@ int main () {
 }
 
 bool run_tests() {
-    const unsigned int tests_number{10};
-    const std::vector<std::vector<int>> a{{2},
-        {1, 2, 5, 4, 3},
-        {1, 2, 3, 4, 5, 6, 10, 9, 8, 7},
-        {2, 0},
-        {5, 7, 8, 8, 10, 23, 60},
-        {4, 8, -3, 73, 2, 7, 25, 1000},
-        {0, -6, 8, 1, 364, 78, -346, 4, 100},
-        {0, 0, 0},
-        {13, 48, 48, -23},
-        {0, -235, 3673, 7, 37, -7, -256, -265246}};
-    const std::array<unsigned int, tests_number> n{1,
-        5,
-        10,
-        2,
-        7,
-        8,
-        9,
-        3,
-        4,
-        8};
-    const std::array<unsigned int, tests_number> correct_result{0,
-        3,
-        6,
-        1,
-        0,
-        8,
-        13,
-        0,
-        3,
-        20};
-    unsigned int result;
+    const unsigned int tests_number{50000};
 
     for (unsigned int i{0}; i < tests_number; i++) {
-        if ((result = count_inversions(a.at(i).data(), n.at(i))) != correct_result.at(i)) {
+        const unsigned int n{get_random_array_number()};
+        int a[n];
+        fill_array(a, n);
+
+        if (simple_count_inversions(a, n) != count_inversions(a, n)) {
             return false;
         }
     }
 
     return true;
+}
+
+unsigned int get_random_array_number() {
+    const unsigned int min{1};
+    const unsigned int max{1000};
+    std::srand(time(0));
+    return min + rand() % (max - min + 1);
+}
+
+void fill_array(int *a, unsigned int n) {
+    const int min{-1000};
+    const int max{1000};
+    std::srand(time(0));
+
+    for (unsigned int i{0}; i < n; i++) {
+        a[i] = min + rand() % (max - min + 1);
+    }
+}
+
+unsigned int simple_count_inversions(const int *a, unsigned int n) {
+    unsigned int result{0};
+
+    for (unsigned int i{0}; i < n - 1; i++) {
+        for (unsigned int j{i + 1}; j < n; j++) {
+            if (a[i] > a[j]) {
+                result++;
+            }
+        }
+    }
+
+    return result;
 }
